bb76c96674dc55e09f10f848e707f7b92c5983e9
eliasdevmin <eliasaraujo1222@outlook.com>
2024-06-06 10:32:55 -0300

    ajuste codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,96 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() {
   runApp(const BookApp());
 }
 
 class BookApp extends StatelessWidget {
-  const BookApp({super.key});
+  const BookApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      theme: ThemeData(
+        primaryColor: Colors.grey[800], // Define a cor primária como cinza escuro
+        scaffoldBackgroundColor: Colors.grey[200], // Define o fundo do esquema como cinza claro
+        textTheme: const TextTheme(
+          headline6: TextStyle(color: Colors.black87), // Define a cor do texto principal como preto
+        ),
+      ),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  String searchQuery = '';
+  int itemCount = 0;
+
+  Future<void> _buscarLivros() async {
+    final url = Uri.https(
+      'www.googleapis.com',
+      'books/v1/volumes',
+      {'q': searchQuery},
+    );
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final jsonResponse = convert.jsonDecode(response.body);
+      setState(() {
+        itemCount = jsonResponse['totalItems'];
+      });
+      print('Número de livros sobre HTTP: $itemCount.');
+    } else {
+      print('A solicitação falhou com status: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          const TextField(),
-          const SizedBox(height: 16),
-          ElevatedButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-            label: const Text('Pesquisar'),
-          ),
-          const SizedBox(height: 16),
-          Text(
-            'Foram encontrados 0 livros sobre 0:',
-            style: Theme.of(context).textTheme.headlineLarge,
-          )
-        ],
+      appBar: AppBar(
+        title: const Text('Livros App'),
       ),
-    ));
-  }
-}
-
-Future<void> operacaoSaaincrona() async {
-  print('Inicio do evento assicrono');
-  await Future.delayed(Duration(seconds: 2));
-  print('FIM DO EVENTO ASSICRONO');
-}
-
-void _buscarLivros() async {
-  final url = Url.https(
-    'www.googleapis.com',
-    'www.googleapis.com',
-    {'q': '{http}'},
-  );
-  final response = await http.get(url);
-
-  if (response.statusCode == 200){
-    final jsoResponde = convert.jsonDecod
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            TextField(
+              controller: _controller,
+              onChanged: (value) {
+                setState(() {
+                  searchQuery = value;
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Pesquisar livros',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: _buscarLivros,
+              icon: const Icon(Icons.search),
+              label: const Text('Pesquisar'),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Foram encontrados $itemCount livros sobre "$searchQuery":',
+              style: Theme.of(context).textTheme.headline6,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
